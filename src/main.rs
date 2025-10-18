1726443fb536a939528589906f67d08aa35556fe
Erick Cestari <erickcestari03@gmail.com>
2025-10-18 11:56:00 -0300

    feat: connect to LQwD node

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("==========================================\n");
 
     // Parse responder public key
-    let rs_pub_hex = "03881f3ab4af423c7a78116ef01d31a65e694c468c23b11271d6caff94f5029f5a";
+    let rs_pub_hex = "0364913d18a19c671bb36dd04d6ad5be0fe8f2894314c36a9db3f03c2d414907e1";
     let rs_pub_bytes = hex::decode(rs_pub_hex)?;
     let responder_public_key = secp256k1::PublicKey::from_slice(&rs_pub_bytes)?;
 
-    let node_address = "192.168.2.12:9735";
+    let node_address = "192.243.215.102:9735";
 
     let mut client = NoiseClient::new(node_address, responder_public_key)?;
 
